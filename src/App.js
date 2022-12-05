71670c90fa3b372133e47fbe4348015a5c388d16
Kyung Han Lee (International Supplier) <v-kyunghlee@microsoft.com>
2022-12-05 10:45:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Azure App Service</h1>
+      <ul>
+        <li>
+          <a href="https://www.denave.com" target="_blank">
+            Denave
+          </a>
+        </li>
+        <li>
+          <a href="https://www.microsoft.com/ko-kr/" target="_blank">
+            Microsoft
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.microsoft.com/ko-kr/microsoft-365/business/compare-all-microsoft-365-business-products-b?&ef_id=CjwKCAiAp7GcBhA0EiwA9U0mtgNceV0B3XuXnMNWhjzXCeWF94aN6q8qQUYD7c_XnpVdq9ss2zFVoxoCL2wQAvD_BwE:G:s&OCID=AIDcmm409lj8ne_SEM_CjwKCAiAp7GcBhA0EiwA9U0mtgNceV0B3XuXnMNWhjzXCeWF94aN6q8qQUYD7c_XnpVdq9ss2zFVoxoCL2wQAvD_BwE:G:s&lnkd=Google_O365SMB_Brand&gclid=CjwKCAiAp7GcBhA0EiwA9U0mtgNceV0B3XuXnMNWhjzXCeWF94aN6q8qQUYD7c_XnpVdq9ss2zFVoxoCL2wQAvD_BwE"
+            target="_blank"
+          >
+            MW
+          </a>
+        </li>
+        <li>
+          <a href="https://azure.microsoft.com/ko-kr/" target="_blank">
+            Azure
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
